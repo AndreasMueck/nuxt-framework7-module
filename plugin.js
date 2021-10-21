5246609695b0d3b84f4294b51c24987a2afe9e36
Andreas Mueck <35628033+AndreasMueck@users.noreply.github.com>
2021-10-21 12:38:54 +0200

    Add files via upload

---
+++ plugin.js
@@ -0,0 +1,15 @@
+// Import Vue
+import { createApp } from 'vue';
+
+// Import Framework7
+import Framework7 from 'framework7/lite-bundle';
+
+// Import Framework7-Vue Plugin
+import Framework7Vue, { registerComponents } from 'framework7-vue/bundle';
+
+// Import Framework7 Styles
+import 'framework7/framework7-bundle.css';
+
+// Init Framework7-Vue Plugin
+Framework7.use(Framework7Vue);
+
